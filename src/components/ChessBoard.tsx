09ef818fc24387ba76d3e1e60b1650d94e29fe81
Aakash Kc <aakashkcx@gmail.com>
2024-06-20 02:28:51 +0100

    add playable chess game

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,4 +1,15 @@
-import { ColorPiece } from "@aakashkcx/chess-engine";
+import {
+  Color,
+  ColorPiece,
+  Index64,
+  Move,
+  getColor,
+  getStart,
+  getTarget,
+  index120To64,
+  index64To120,
+} from "@aakashkcx/chess-engine";
+import { useMemo, useState } from "react";
 
 import { ChessSquare } from "./ChessSquare";
 
@@ -6,13 +17,49 @@ import "./ChessBoard.css";
 
 interface ChessBoardProps {
   board: ColorPiece[];
+  player: Color;
+  color: Color;
+  moves: Move[];
+  makeMove: (start: Index64, target: Index64) => boolean;
 }
 
-export function ChessBoard({ board }: ChessBoardProps) {
+export function ChessBoard({ board, color, moves, makeMove }: ChessBoardProps) {
+  const [selected, setSelected] = useState<Index64>();
+
+  const moveSquares = useMemo<Set<Index64>>(() => {
+    if (selected === undefined) return new Set();
+    const targets = moves
+      .filter((move) => getStart(move) === index64To120(selected))
+      .map(getTarget)
+      .map(index120To64);
+    return new Set(targets);
+  }, [selected, moves]);
+
+  function selectSquare(index: Index64) {
+    return function () {
+      const piece = board[index];
+      if (selected === undefined) {
+        if (getColor(piece) === color) return setSelected(index);
+      } else {
+        if (selected === index) return setSelected(undefined);
+        if (getColor(piece) === color) return setSelected(index);
+        if (moveSquares.has(index)) makeMove(selected, index);
+        setSelected(undefined);
+      }
+    };
+  }
+
   return (
     <div className="chess-board">
       {board.map((piece, index) => (
-        <ChessSquare key={index} index={index} piece={piece} />
+        <ChessSquare
+          key={index}
+          index={index}
+          piece={piece}
+          selected={index == selected}
+          move={moveSquares.has(index)}
+          onClick={selectSquare(index)}
+        />
       ))}
     </div>
   );
--- src/components/ChessGame.tsx
+++ src/components/ChessGame.tsx
@@ -1,11 +1,26 @@
-import { useChessGame } from "../hooks/useChessGame";
+import { Color } from "@aakashkcx/chess-engine";
 
+import { useChessGame } from "../hooks/useChessGame";
 import { ChessBoard } from "./ChessBoard";
 
 import "./ChessGame.css";
 
 export function ChessGame() {
-  const { board } = useChessGame();
+  const { board, fen, color, moves, ply, makeMove, takeBack } = useChessGame();
 
-  return <ChessBoard board={board} />;
+  return (
+    <div>
+      <ChessBoard
+        board={board}
+        player={Color.White}
+        color={color}
+        moves={moves}
+        makeMove={makeMove}
+      />
+      <div>{fen}</div>
+      <button onClick={() => takeBack()} disabled={ply < 1}>
+        Take Back
+      </button>
+    </div>
+  );
 }
