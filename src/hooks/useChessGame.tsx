f2aa9426755c93f7f75c27e3c29050d14926d2a7
Aakash Kc <aakashkcx@gmail.com>
2024-06-20 02:28:00 +0100

    add game data projections, state, methods

--- src/hooks/useChessGame.tsx
+++ src/hooks/useChessGame.tsx
@@ -1,21 +1,84 @@
-import { ChessGame, Move } from "@aakashkcx/chess-engine";
+import {
+  ChessGame,
+  Color,
+  ColorPiece,
+  Index64,
+  Move,
+  getStart,
+  getTarget,
+  index64To120,
+} from "@aakashkcx/chess-engine";
 import { useRef, useState } from "react";
 
+/**
+ * A projection of the chess game data for use in React state.
+ */
+interface GameData {
+  /** The chess board, of length 64. */
+  board: ColorPiece[];
+  /** The Forsyth–Edwards Notation (FEN) string. */
+  fen: string;
+  /** The next color to move. */
+  color: Color;
+  /** The array of legal moves that can be made. */
+  moves: Move[];
+  /** The number of plies played. */
+  ply: number;
+}
+
+/**
+ * Get the game data of a chess game.
+ * @param game The chess game.
+ * @returns A projection of the chess game data.
+ */
+function getGameData(game: ChessGame): GameData {
+  return {
+    board: game.getBoard(),
+    fen: game.getFEN(),
+    color: game.activeColor,
+    moves: game.generateMoves(),
+    ply: game.ply,
+  };
+}
+
+/**
+ * A custom React hook which returns a stateful chess game.
+ */
 export function useChessGame() {
   const ref = useRef(new ChessGame());
   const gameRef = ref.current;
 
-  const [board, setBoard] = useState(gameRef.getBoard());
+  const [game, setGame] = useState(getGameData(gameRef));
 
-  function makeMove(move: Move) {
-    gameRef.makeMove(move);
-    setBoard(gameRef.getBoard());
+  /**
+   * Make a move on the chessboard.
+   * @param start The start square.
+   * @param target The target square.
+   * @returns Whether the move was legal and therefore completed.
+   */
+  function makeMove(start: Index64, target: Index64): boolean {
+    start = index64To120(start);
+    target = index64To120(target);
+    for (const move of game.moves) {
+      if (getStart(move) === start && getTarget(move) === target) {
+        const legal = gameRef.makeMove(move);
+        setGame(getGameData(gameRef));
+        return legal;
+      }
+    }
+    return false;
   }
 
-  function takeBack() {
+  /**
+   * Take back the move on the chessboard.
+   * @param twice Whether to take back two moves (same side) or one move (switch sides).
+   */
+  function takeBack(twice: boolean = false) {
+    if (game.ply < (twice ? 2 : 1)) return alert("Cannot take back!");
     gameRef.takeBack();
-    setBoard(gameRef.getBoard());
+    if (twice) gameRef.takeBack();
+    setGame(getGameData(gameRef));
   }
 
-  return { gameRef, board, makeMove, takeBack };
+  return { gameRef, ...game, makeMove, takeBack };
 }
