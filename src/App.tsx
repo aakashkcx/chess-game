9884b8032e42bb06cf3576757ac2b000700d0398
Aakash Kc <aakashkcx@gmail.com>
2024-07-12 04:52:33 +0100

    remove component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import { ChessAI } from "./routes/ChessAI";
 import { ChessAnalysis } from "./routes/ChessAnalysis";
 import { ChessLocal } from "./routes/ChessLocal";
 import { Menu } from "./routes/Menu";
@@ -9,7 +8,6 @@ function App() {
   return (
     <Routes>
       <Route index element={<Menu />} />
-      <Route path="ai" element={<ChessAI />} />
       <Route path="local" element={<ChessLocal />} />
       <Route path="analysis" element={<ChessAnalysis />} />
       <Route path="*" element={<Navigate to="/" replace />} />
